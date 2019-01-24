352d68bbca836af1a6d073d6b688b41f49f98eb5
NerdyPepper <akshayoppiliappan@gmail.com>
2019-01-24 21:50:39 +0530

    system is input in the form A|B

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,27 @@
-const SIZE: usize = 4;
+const SIZE: usize = 3;
 
 fn main() {
-    let mut system = [
-        [1f32, 1f32, 1f32, 1f32],
-        [2f32, 3f32, 4f32, 5f32],
-        [5f32 ,6f32, 7f32, 8f32],
-        [8f32 ,9f32, 10f32, 11f32]
-    ];
+    let mut system: [[i32; 4]; 3] = [
+        [1, 3, 1, 9],
+        [1, 1, -1, 1],
+        [3, 11, 5, 35]
+    ] ;
 
-    // make echelon form
     for i in 0..SIZE-1 {
         for j in i..SIZE-1 {
-        let factor = system[j + 1][i] as f32 / system[i][i] as f32;
-        println!("Factor: {}", factor);
-            for k in i..SIZE {
-                system[j + 1][k] -= factor * system[i][k];
-                println!("{:?}", system);
+            let factor = system[j + 1][i] as f32 / system[i][i] as f32;
+            if system[i][i] == 0 {
+                println!("Skipped!");
+                continue;
+            } else {
+                println!("Factor: {}", factor);
+                for k in i..SIZE+1 {
+                    system[j + 1][k] -= (factor * system[i][k] as f32) as i32;
+                    println!("{:?}", system);
+                }
             }
         }
     }
     println!("\nFinal system:\n{:?}", system);
 }
+
