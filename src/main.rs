576874d3f287e159b496e8b7349b9ccde8399a7c
NerdyPepper <akshayoppiliappan@gmail.com>
2019-01-31 20:39:40 +0530

    implement Gauss-Jordan elimination, A|B format printing

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,15 @@ const SIZE: usize = 3;
 
 fn main() {
     let mut system: [[f32; SIZE + 1]; SIZE] = [
-        // A , A    , A    , B
-        [1.0 , 3.0  , 1.0  , 9.0]  ,
-        [1.0 , 1.0  , -1.0 , 1.0]  ,
-        [3.0 , 11.0 , 5.0  , 35.0]
+        // A , A   , A    , B
+        [1.0 , 3.0 , 4.0  , 3.0]  ,
+        [2.0 , 7.0 , 3.0 , -7.0]  ,
+        [2.0 , 8.0 , 6.0  , -4.0]
     ] ;
 
+    println!("Original system: ");
     print_2d(system);
+
     for i in 0..SIZE-1 {
         for j in i..SIZE-1 {
             if system[i][i] == 0.0 {
@@ -22,23 +24,25 @@ fn main() {
         }
     }
 
+    println!("\n{}", "-".repeat(20));
+    println!("Echelon form:");
     print_2d(system);
 
     for i in (1..SIZE).rev() {
-        for j in (i..SIZE).rev() {
-            if system[i][i] == 0.0 {
-                continue;
-            } else {
+        if system[i][i] == 0.0 {
+            continue;
+        } else {
+            for j in (1..i+1).rev() {
                 let factor = system[j - 1][i] as f32 / system[i][i] as f32;
-                println!("Factor: {}", factor);
-                for k in (i..SIZE+1).rev() {
+                for k in (0..SIZE+1).rev() {
                     system[j - 1][k] -= factor * system[i][k] as f32;
-                    print_2d(system);
                 }
             }
         }
     }
 
+    println!("\n{}", "-".repeat(20));
+    println!("Gaussian eliminated:");
     print_2d(system);
 }
 
@@ -46,6 +50,9 @@ fn print_2d( system: [[f32; SIZE + 1]; SIZE] ) {
     println!();
     for i in 0..SIZE {
         for j in 0..SIZE+1 {
+            if j == SIZE {
+                print!("| ");
+            }
             print!("{:02} ", system[i][j]);
         }
         println!();
